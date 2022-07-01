a00d93f322e7ca62bcc96641f156a8d21aefed09
Filipe Lima <cimofelipe4@gmail.com>
2022-06-30 21:22:44 -0300

    Finalizado exercicio 01 dia 4.3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-01.js
@@ -0,0 +1,5 @@
+let fatorial = 10
+let resultado = fatorial
+for (let count = 1; count < fatorial; count += 1){
+    resultado *= count
+}
--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/script.js
+++
@@ -1,8 +0,0 @@
-const n = 9
-let resultado = 100
-
-for (let i = 0; i <= n; i+=1){
-    resultado -= 1
-}
-
-console.log(resultado)
