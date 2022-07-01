50e46fc456c1a6d4fc241cb3ebae9f1b9894ab4d
Filipe Lima <cimofelipe4@gmail.com>
2022-06-30 22:09:09 -0300

    Exercicios transformados em funções

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-01.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-01.js
@@ -1,5 +1,8 @@
-let fatorial = 10
-let resultado = fatorial
-for (let count = 1; count < fatorial; count += 1){
-    resultado *= count
+function returnFactorial(factorial){
+    let result = factorial
+    for (let count = 1; count < factorial; count += 1){
+        result *= count
+    }
+    return result
 }
+returnFactorial(4)
