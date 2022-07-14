5bc3dd3330129cc177f20eeab23cc902ee10cab9
Filipe Lima Acer <cimofelipe4@gmail.com>
2022-07-14 18:59:18 -0300

    Finalizado parte 1 dos exercicios

---
+++ fundamentos/bloco-07-introducao-jses6-testes-unitarios/part-1.js
@@ -0,0 +1,21 @@
+// Exercicio 1
+// const testingScope = (escopo) => {
+//   let ifScope = "";
+//   if (escopo === true) {
+//     ifScope = "Não devo ser utilizada fora do meu escopo (if)";
+//     ifScope = `${ifScope}. ${" ótimo, fui utilizada no escopo !"}`;
+//     console.log(ifScope);
+//   } else {
+//     const elseScope = "Não devo ser utilizada fora meu escopo (else)";
+//     console.log(elseScope);
+//   }
+// };
+// testingScope(true);
+
+// exercicio 2
+// const array = [13, 3, 4, 10, 7, 2];
+// const oddsAndEvens = (numbers) => {
+//     return `Os números ${numbers.sort((a, b) => a - b)} se encontram ordenados de forma crescente`;
+// }
+
+// console.log(oddsAndEvens(array));
