4e9c018d483d770ee06892a8b8ae560c39ad88fd
Filipe Lima <cimofelipe4@gmail.com>
2022-07-27 15:26:24 -0300

    Refatorado exercicio 1 e 2

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-reduce/exercise1.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-reduce/exercise1.js
@@ -1,7 +1,6 @@
 const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
 
-function flatten(matriz) {
-    return matriz.reduce((acc, curr) => acc.concat(curr), [])
+function flatten(arrays) {
+    return arrays.reduce((acc, array) => acc.concat(array), [])
 }
-
-console.log(flatten(arrays));
+console.log(flatten(arrays))
