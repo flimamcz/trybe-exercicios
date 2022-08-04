3c3777f997d6174100db7ff2f6394ed3ea4bc362
Filipe Lima <cimofelipe4@gmail.com>
2022-08-04 19:15:45 -0300

    exercise 07 confuso

---
+++ front-end/bloco-09-javascript-e-testes-assicronos/dia-01-javascript-e-callbacks/exercise7.js
@@ -0,0 +1,13 @@
+// const letterUppercase = ((str) => str.toUpperCase()) 
+
+// const uppercase = (str, callback) => {
+//   setTimeout(() => {
+//     const strUpper = callback(str);
+//   }, 500);
+
+// };
+
+// console.log(uppercase('filipe', letterUppercase));
+
+
+//EXERCICIO NÃO COMPREENDIDO
