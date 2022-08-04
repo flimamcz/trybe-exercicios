83f76ef4c9aee95f1926f5a76c6fe22544cb7cc6
Filipe Lima <cimofelipe4@gmail.com>
2022-08-04 19:06:14 -0300

    exercicio 1 ao 6 finalizado

---
+++ front-end/bloco-09-javascript-e-testes-assicronos/dia-01-javascript-e-callbacks/exercise6.js
@@ -0,0 +1,33 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// definição da função sendMarsTemperature...
+
+function handleError(){
+  console.log('Error getting temperature: Robot is busy')
+}
+
+function sendMarsTemperature(callback, error) {
+  setTimeout(() => {
+    if(isNaN(getMarsTemperature())) {
+      error()
+    } else {
+      callback(getMarsTemperature());
+    }
+  }, messageDelay());
+}
+
+sendMarsTemperature(temperatureInFahrenheit, handleError); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet, handleError); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
